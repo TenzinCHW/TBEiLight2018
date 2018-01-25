35eeca5a5a451d3771408365e9ba3d7f7e2cf9ef
TenzinCHW <tenzin_chan@mymail.sutd.edu.sg>
2018-01-26 00:41:50 +0800

    Added local functions for HitQueue struct and added calls to transmit messages

--- LampControl/state.cpp
+++ LampControl/state.cpp
@@ -1,6 +1,4 @@
 #include "state.h"
-#include "messaging.h"
-#include "comms.h"
 
 bool indiv_var_set = false;
 bool globals_set = false;
@@ -15,6 +13,7 @@ HitQueue hits;
 float wavelength;
 uint16_t period;
 byte ID[3];
+byte msg_buf[PACKET_SZ];
 
 void main_loop() {
   if (!(indiv_var_set && globals_set)) {
@@ -22,20 +21,21 @@ void main_loop() {
       uint8_t num_try = 0;
       byte* req = make_indiv_req(ID);
       while (!indiv_var_set && (num_try < RETRY_TIMES)) {
-        // TODO send req
-        delay(100); // TODO not a good idea to use delays
+        broadcast(1, req);
+        delay(10);  // TODO may have to adjust this...
         read_and_handle();
+        num_try++;
       }
       if (!indiv_var_set) power_down(); // Still haven't set up individual configurations after checking, go back to sleep
     } else if (!globals_set && millis() - time_since_last_glob_req > 2000) {
-      byte* req = make_glob_req();  // TODO send this
+      broadcast(1, make_glob_req());
       read_and_handle();
       time_since_last_glob_req = millis();
     }
   }
   else {
     // TODO try to listen for reset message from master - should be encrypted (low priority)
-    // TODO try to receive drum hit/hello, add to drum hit when received, set last_hello to millis()
+    read_and_handle();  // try to receive drum hit/hello, add to drum hit when received, set last_hello to millis()
     // TODO update LEDs
     if (millis() - last_hello > 30000) {
       reset_vars();
@@ -60,26 +60,33 @@ void reset_vars() { // resets all variables that need to be reset
   for (uint8_t i = 0; i < 32; i++) msg_buf[i] = NULL;
   wavelength = 0;
   period = 0;
-  while (hits.counter > 0) pop_hit(&hits);
+  while (hits.counter > 0) hits.pop_hit();
   Timer1.detachInterrupt();
   // Do not reset ID and do not stop listening on radio
   // TODO reset other variables
 }
 
 void power_down() { // powers down for a few seconds
-  // TODO power down radio
   LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,
                 SPI_OFF, USART0_OFF, TWI_OFF);  // TODO choose between this or the one below to save power
   //  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
-  // TODO power up radio
 }
 
 void read_and_handle() {
-  if (radio.available()) {
-    // TODO read message into variable msg_buf here
-    if (is_relay && to_be_relayed(msg_buf)) // TODO forward msg_buf
-      if (is_hello(msg_buf)) last_hello = millis();
-    switch (get_msg_type(msg_buf)) {
+  if (read_if_avail(msg_buf)) {
+    uint8_t msg_type = get_msg_type(msg_buf);
+    if (is_relay && to_be_relayed(msg_buf)) {
+      set_relay_bit(msg_buf);
+      switch (msg_type) {  // check what type of msg - info or hello or drum hit send on ADDR0, req or ack send on ADDR1
+        case SETUP_REQ_MSG : broadcast(1, msg_buf); break;
+        case SETUP_ACK : broadcast(1, msg_buf); break;
+        case DRUM_HIT_MSG : broadcast(0, msg_buf); break;
+        case SETUP_MSG : broadcast(0, msg_buf); break;
+        case HELLO_MSG : broadcast(0, msg_buf); break;
+        default : broadcast(0, msg_buf); break;
+      }
+    }
+    switch (msg_type) {
       case SETUP_MSG :
         if (is_global(msg_buf)) global_setup();
         else if (addressed_to_id(msg_buf, ID)) indiv_setup();
@@ -88,6 +95,8 @@ void read_and_handle() {
         last_hello = millis();
         add_drum_hit(&hits, get_drum_id(msg_buf), get_hit_intensity(msg_buf), get_hit_counter(msg_buf));
         break;
+      case HELLO_MSG :
+        last_hello = millis();
     }
   }
 }
@@ -99,7 +108,7 @@ void indiv_setup() {
   Timer1.initialize(1000000);
   Timer1.attachInterrupt(remove_old_hits);
   indiv_var_set = true;
-  byte* ack = make_ack(ID); // TODO send this
+  broadcast(1, make_ack(ID));
 }
 
 void global_setup() {
@@ -119,33 +128,18 @@ void global_setup() {
   globals_set = true;
 }
 
-void push_hit(HitQueue* queue, DrumHit hit) {
-  while (queue->counter == MAX_HITS) { // TODO this may cause timing problems if queue is full and hits do not expire for a while
-    remove_old_hits();
-  }
-  queue->hits[(queue->head + queue->counter) % MAX_HITS] = hit;
-  queue->counter++;
-}
-
-void pop_hit(HitQueue* queue) {
-  uint16_t ret = queue->head;
-  queue->head = (ret + 1) % MAX_HITS;
-  queue->counter--;
-  return queue->hits[ret];
-}
-
 void add_drum_hit(HitQueue* queue, uint8_t drum_id, float intensity, uint16_t counter) {
   DrumHit hit;
   hit.drum_id = drum_id;
   hit.intensity = intensity;
   hit.incoming_time = millis();
   hit.counter = get_hit_counter(counter);
-  push_hit(queue, hit);
+  hits.push_hit(hit);
 }
 
 void remove_old_hits() {
   while (hits.counter > 0) {
-    if (millis() - hits.hits[hits.head].incoming_time > expiry_time) pop_hit(&hits);
+    if (millis() - hits.hits[hits.head].incoming_time > expiry_time) hits.pop_hit();
     else break;
   }
 }
--- LampControl/state.h
+++ LampControl/state.h
@@ -2,6 +2,8 @@
 #include <TimerOne.h>
 #include "LowPower.h"
 #include "messaging.h"
+#include "comms.h"
+#include "led.h"
 
 #define MAX_HITS 80
 #define NUM_OF_DRUMS 4
@@ -20,10 +22,27 @@ struct Drum {
   uint8_t colour[3];
 };
 
+void remove_old_hits();
+
 struct HitQueue {
   DrumHit hits[MAX_HITS];
   uint16_t head;
   uint16_t counter;
+
+  void pop_hit() {
+    uint16_t ret = this->head;
+    this->head = (ret + 1) % MAX_HITS;
+    this->counter--;
+    return this->hits[ret];
+  }
+
+  void push_hit(DrumHit hit) {
+    while (this->counter == MAX_HITS) { // this may cause timing problems if queue is full and hits do not expire for a while
+      remove_old_hits();
+    }
+    this->hits[(this->head + this->counter) % MAX_HITS] = hit;
+    this->counter++;
+  }
 };
 
 void main_loop();
@@ -32,7 +51,4 @@ void power_down();
 void read_and_handle();
 void indiv_setup();
 void global_setup();
-void push_hit(HitQueue* queue, DrumHit hit);
-void pop_hit(HitQueue* queue);
 void add_drum_hit(HitQueue* queue, uint8_t drum_id, float intensity, uint16_t counter);
-void remove_old_hits();
