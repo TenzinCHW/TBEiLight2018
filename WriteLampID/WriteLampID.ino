fec9306f3a1dd5ec8c45e69278dadb7d465e5b65
TenzinCHW <tenzin_chan@mymail.sutd.edu.sg>
2018-02-01 00:27:57 +0800

    Added code for writing IDs to Arduinos

---
+++ WriteLampID/WriteLampID.ino
@@ -0,0 +1,16 @@
+#include <EEPROM.h>
+
+void setup() {
+  // Use this to write the IDs to EEPROM
+  uint16_t ID = 0;
+  EEPROM.write(0, ID >> 8);
+  EEPROM.write(1, ID);
+  
+  uint16_t check = EEPROM.read(0) << 8 | EEPROM.read(1);
+  if (check != ID) Serial.println(F("ID was not written to EEPROM correctly"));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
