497af8eae0ddb5c0ebdc249cff03d3477a4f065f
TenzinCHW <tenzin_chan@mymail.sutd.edu.sg>
2018-02-09 01:42:56 +0800

    Added light setup and ifndef for led.h

--- LampControl/led.cpp
+++ LampControl/led.cpp
@@ -1,5 +1,14 @@
 #include "led.h"
 
+CRGB lights[NUM_LED];
+extern State state;
+
 void setup_lights() {
-//  FastLED.addLeds<CHIPSET, LED_PIN>(lights, strip_cuml[0], strip_len[0]);
+  FastLED.addLeds<CHIPSET, LED_PIN>(lights, NUM_LED);
+}
+
+void set_rgb(CRGB* lights, State state) {
+  // TODO wave equation goes here. Add up values for each drum.
+  // use lights[i].setRGB(r_val, g_val, b_val);
+  // where r_val, g_val and b_val are the values of red, green and blue respectively.
 }
--- LampControl/led.h
+++ LampControl/led.h
@@ -1,9 +1,15 @@
+#ifndef LED
+#define LED 1
+
 #include <FastLED.h>
+#include "state.h"
 
 #define CHIPSET UCS1903B
 #define NUM_LED 4
 #define LED_PIN 3
 #define CLOCK_PIN 5
 
-extern CRGB lights[NUM_LED];
+void setup_lights();
+void set_rgb(); // TODO test
 
+#endif
