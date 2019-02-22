420a3c68da187480631b1af5e1fb8083e4493ff4
Therese Lau <thereselau1902@gmail.com>
2019-02-22 17:54:18 +0800

    Updated comms.h and comms.cpp for new PCB

--- Drums/NonMasterDrum/comms.cpp
+++ Drums/NonMasterDrum/comms.cpp
@@ -2,12 +2,13 @@
 #include "printf.h"
 #include <util/atomic.h>
 
-RF24 radio(10, 9);
+RF24 radio(7, 8);
 
 const unsigned char ADDR1[5]  = {0xb1, 0x41, 0x29, 0x75, 0x93};
 const unsigned char ADDR0[5]  = {0xb0, 0x41, 0x29, 0x75, 0x93};
 
 void startup_nRF() {
+  radio_on();
   radio.begin();
   printf_begin();
   radio.setDataRate(RF24_2MBPS);
@@ -22,13 +23,23 @@ void startup_nRF() {
   radio.printDetails();
 }
 
+void radio_on() {
+  pinMode(POWER_PIN, OUTPUT);
+  digitalWrite(POWER_PIN, HIGH);
+}
+
+void radio_off() {
+  pinMode(POWER_PIN, OUTPUT);
+  digitalWrite(POWER_PIN, LOW);
+}
+
 bool read_if_avail(uint8_t* buf) {
   if (radio.available()) {
     if (radio.getDynamicPayloadSize() < 1) {
       return false;
     }
     read_and_flush(buf);
-    print_buffer(buf, PACKET_SZ);
+    //    print_buffer(buf, PACKET_SZ);
     return true;
   } else {
     return false;
@@ -42,6 +53,8 @@ void read_and_flush(uint8_t* buf) {
 
 void broadcast(uint8_t addr, byte* msg, uint8_t sz) {
   ATOMIC_BLOCK(ATOMIC_FORCEON) {
+    //  Serial.print(F("Sending this: "));
+    //  print_buffer(msg, PACKET_SZ);
     radio.stopListening();
     switch (addr) {
       case 0 : radio.openWritingPipe(ADDR0);
--- Drums/NonMasterDrum/comms.h
+++ Drums/NonMasterDrum/comms.h
@@ -4,10 +4,14 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <DigitalIO.h>
 
 #define PACKET_SZ 32
+#define POWER_PIN 13
 
 void startup_nRF();
+void radio_on();
+void radio_off();
 bool read_if_avail(uint8_t* buf);
 void read_and_flush(uint8_t* buf);
 void broadcast(uint8_t addr, byte* msg, uint8_t sz);
